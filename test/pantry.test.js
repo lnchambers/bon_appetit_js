ea22bc65140ebec5d74f91f06b91243e43f20d83
Luke Chambers <lukenatchambers@gmail.com>
2018-05-04 11:49:09 -0600

    Struggle bug

--- test/pantry.test.js
+++ test/pantry.test.js
@@ -35,5 +35,23 @@ describe("Pantry", () => {
 
       expect(pantry.shoppingList).to.eql({"Cheese": 20, "Flour": 20})
     })
+
+    it("can add moar ingredients to shopping list", () => {
+      recipe.addIngredient("Cheese", 20)
+      recipe.addIngredient("Flour", 20)
+
+      expect(recipe.ingredients).to.eql({"Cheese": 20, "Flour": 20})
+      pantry.addToShoppingList(recipe)
+
+      expect(pantry.shoppingList).to.eql({"Cheese": 20, "Flour": 20})
+
+      const r = new Recipe("Spaghetti")
+      r.addIngredient("Noodles", 10)
+      r.addIngredient("Sauce", 10)
+      r.addIngredient("Cheese", 5)
+      pantry.addToShoppingList(r)
+
+      expect(pantry.shoppingList).to.eql({"Cheese": 25, "Flour": 20, "Noodles": 10, "Sauce": 10})
+    })
   })
 })
